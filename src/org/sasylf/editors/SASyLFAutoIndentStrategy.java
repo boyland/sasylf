c30fc9653ac9a5c5d6681a9f2884a758a442a654
boyland.john <boyland.john@gmail.com>
2013-08-24 20:22:41 +0000

    Added more syntactic error productions

--- src/org/sasylf/editors/SASyLFAutoIndentStrategy.java
+++ src/org/sasylf/editors/SASyLFAutoIndentStrategy.java
@@ -63,7 +63,6 @@ public class SASyLFAutoIndentStrategy extends SASyLFIndentStrategy {
         System.out.println("bad event replayed: " + e);
         // muffle
       }
-      // TODO modify caret.
     }  
   };
   
