8b6c4a42572dd9b897c7ecd69cd4ac50714b78e9
boyland.john <boyland.john@gmail.com>
2014-02-15 15:45:23 +0000

    Fix Issue #43

--- src/edu/cmu/cs/sasylf/ast/Theorem.java
+++ src/edu/cmu/cs/sasylf/ast/Theorem.java
@@ -164,6 +164,15 @@ public class Theorem extends RuleLike {
 		if (ErrorHandler.getErrorCount() > oldErrorCount) {
 		  return;
 		}
+
+		/*
+    if (andTheorem != null) {
+      andTheorem.addToMap(ctx);
+    }*/
+    ctx.recursiveTheorems = new HashMap<String, Theorem>();
+    firstInGroup.addToMap(ctx);
+
+    ctx.bindingTypes = new HashMap<String, List<ElemType>>();
 		
 		if (assumes != null) {
 		  ctx.innermostGamma = assumes;
@@ -192,13 +201,6 @@ public class Theorem extends RuleLike {
 		ctx.outputVars.addAll(theoremTerm.getFreeVariables());
 		ctx.outputVars.removeAll(ctx.inputVars);
 		
-		/*
-		if (andTheorem != null) {
-			andTheorem.addToMap(ctx);
-		}*/
-    ctx.recursiveTheorems = new HashMap<String, Theorem>();
-		firstInGroup.addToMap(ctx);
-
 		Derivation.typecheck(this, ctx, derivations);
 		
 		} catch (SASyLFError e) {
