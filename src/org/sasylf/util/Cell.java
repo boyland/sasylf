48f1f86b613da75f6cc259ca5ead3de85ac27815
boyland.john <boyland.john@gmail.com>
2014-02-23 20:13:24 +0000

    utility methods

---
+++ src/org/sasylf/util/Cell.java
@@ -0,0 +1,18 @@
+package org.sasylf.util;
+
+public class Cell<T> {
+
+  private T value;
+  
+  public Cell(T initial) {
+    value = initial;
+  }
+
+  public T get() {
+    return value;
+  }
+  
+  public void set(T next) {
+    value = next;
+  }
+}
