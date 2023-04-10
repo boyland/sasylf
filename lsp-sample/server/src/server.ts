4102fd4e98b4cb3bed2d4bdd69a58ca2998cfd97
Douglas Chen <dchen3@andrew.cmu.edu>
2023-04-10 15:45:02 -0400

    removed template code

--- lsp-sample/server/src/server.ts
+++ lsp-sample/server/src/server.ts
@@ -191,33 +191,7 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 
 connection.onDidChangeWatchedFiles(change => {
   // Monitored files have change in VSCode
-  connection.console.log('We received an file change event');
   console.log('We received an file change event');
-});
-
-// This handler provides the initial list of the completion items.
-connection.onCompletion(
-    (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-      // The pass parameter contains the position of the text document in
-      // which code complete got requested. For the example we ignore this
-      // info and always provide the same completion items.
-      return [
-        {label : 'TypeScript', kind : CompletionItemKind.Text, data : 1},
-        {label : 'JavaScript', kind : CompletionItemKind.Text, data : 2}
-      ];
-    });
-
-// This handler resolves additional information for the item selected in
-// the completion list.
-connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-  if (item.data === 1) {
-    item.detail = 'TypeScript details';
-    item.documentation = 'TypeScript documentation';
-  } else if (item.data === 2) {
-    item.detail = 'JavaScript details';
-    item.documentation = 'JavaScript documentation';
-  }
-  return item;
 });
 
 // Looks for quickfixes in the `quickfixes` map and returns them if they exist
