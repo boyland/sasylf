1d204651ac56c14cf4c32220effd17bc34ba7696
Douglas Chen <dchen3@andrew.cmu.edu>
2023-04-20 17:46:37 -0400

    changed lsp-sample to lsp-sasylf

--- lsp-sasylf/client/src/test/helper.ts
+++ lsp-sasylf/client/src/test/helper.ts
@@ -12,11 +12,11 @@ export let documentEol: string;
 export let platformEol: string;
 
 /**
- * Activates the vscode.lsp-sample extension
+ * Activates the vscode.lsp-sasylf extension
  */
 export async function activate(docUri: vscode.Uri) {
 	// The extensionId is `publisher.name` from package.json
-	const ext = vscode.extensions.getExtension('lsp-sample')!;
+	const ext = vscode.extensions.getExtension('lsp-sasylf')!;
 	await ext.activate();
 	try {
 		doc = await vscode.workspace.openTextDocument(docUri);
