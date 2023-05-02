991291397dbfae4e81cdb188b4893963a3938488
rdasanoor <rdasanoor@gmail.com>
2023-05-02 15:42:05 -0400

    removed unnecessary import

--- src/edu/cmu/cs/sasylf/Proof.java
+++ src/edu/cmu/cs/sasylf/Proof.java
@@ -1,6 +1,5 @@
 package edu.cmu.cs.sasylf;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
