c3b49759511ebd2555df8c6edd3a4fc97c2759ae
Douglas Chen <dchen3@andrew.cmu.edu>
2023-07-10 20:50:38 -0400

    register the extension for sasylf

--- lsp-sasylf/client/src/extension.ts
+++ lsp-sasylf/client/src/extension.ts
@@ -29,7 +29,7 @@ export function activate(context: ExtensionContext) {
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+		documentSelector: [{ scheme: 'file', language: 'sasylf' }],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
