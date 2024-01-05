c789a514df0f9521b3d59741bb25dff01fb4a21a
rdasanoor <rdasanoor@gmail.com>
2024-01-05 17:19:25 -0500

    modified export codoe

--- lsp-sasylf/electron_app/src/components/export.tsx
+++ lsp-sasylf/electron_app/src/components/export.tsx
@@ -3,50 +3,7 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-
-type line = {
-	conclusion: string;
-	name: string;
-	rule: string;
-	premises: string[];
-};
-
-function theoremHelper(proofNode) {
-	const containers = proofNode.getElementsByClassName("conclusion");
-	if (containers.length === 0) return [];
-	const container = containers[containers.length - 1];
-	const conclusion = container.getElementsByTagName("span")[0].textContent;
-	const name = container.getElementsByTagName("input")[0].value;
-	const rules = proofNode.getElementsByClassName("rule");
-	const rule = rules[rules.length - 1].textContent;
-
-	let content: line[] = [];
-	let premises: string[] = [];
-	const premiseNodes = proofNode.getElementsByClassName("premises");
-	for (const premise of premiseNodes) {
-		const prev = theoremHelper(premise);
-		if (prev.length > 0) {
-			premises.push(prev[prev.length - 1].name);
-			content = content.concat(prev);
-		}
-	}
-
-	content.push({ conclusion, name, rule, premises });
-	return content;
-}
-
-function createTheorem(theoremName: string, dom) {
-	const root = dom.getElementsByClassName("root-node")[0];
-	const nodes = theoremHelper(root);
-	const conclusion = nodes[0].conclusion;
-	let content = `theorem ${theoremName} : ${conclusion}.\n`;
-	for (const node of nodes) {
-		content += `${node.name}: ${node.conclusion} by rule ${node.rule.trim()}${
-			node.premises.length === 0 ? "" : ` on ${node.premises.join(", ")}`
-		}\n`;
-	}
-	return content;
-}
+import { createTheorem } from "./utils";
 
 interface ExportProps {
 	show: boolean;
@@ -55,9 +12,9 @@ interface ExportProps {
 }
 
 export default function Export(props: ExportProps) {
-	function handleExport(event) {
+	const handleExport: React.FormEventHandler<HTMLFormElement> = (event) => {
 		event.preventDefault();
-		const formData = new FormData(event.target);
+		const formData = new FormData(event.target as HTMLFormElement);
 
 		const formJson = Object.fromEntries(formData.entries());
 		const theoremName: string = formJson.theoremName as string;
@@ -69,7 +26,7 @@ export default function Export(props: ExportProps) {
 			(window as any).electronAPI.saveFile(theorem);
 		}
 		props.onHide();
-	}
+	};
 
 	return (
 		<Modal show={props.show} onHide={props.onHide}>
