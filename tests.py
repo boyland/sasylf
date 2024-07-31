50accf450d8bf06fc77e9168b8725d3db66d1e4d
Alex Kozik <alex.kozik@yahoo.com>
2024-07-31 09:51:45 -0400

    Fixed tests.py

--- tests.py
+++ tests.py
@@ -7,6 +7,14 @@ def remove_all(l, item):
   while item in l:
     l.remove(item)
 
+def list_files(directory):
+    files = []
+    for item in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, item)):
+            files.append(f"./regression/{item}")
+    return files
+
+
 # whether it prints the entire error message or just the line number
 verbose = False
 
@@ -14,17 +22,14 @@ if len(sys.argv) > 1:
   if "-v" in sys.argv:
     verbose = True
 
-small_tests = []
+small_tests = list_files("./regression")
 module_tests = []
 
 for root, dirs, files in os.walk("./regression"):
   for dir_name in dirs:
-    #print(f"dir_name: {dir_name}")
-    test_file = os.path.join(root, dir_name, "test.slf") # TODO: maybe switch to main.slf for the main file in the test
+    test_file = os.path.join(root, dir_name, "test.slf")
     if os.path.exists(test_file):
       module_tests.append(dir_name)
-  for file_name in files:
-    small_tests.append(os.path.join(root, file_name))
 
 small_tests.sort()
 module_tests.sort()
@@ -52,7 +57,7 @@ def get_error_lines(test: str):
   lines = read_file_and_count_lines(test)
   
   for i, line in enumerate(lines):
-    if "//!" in line:
+    if "//!" in line or "/*!" in line:
       error_lines.append(i + 1)
 
   return error_lines
@@ -62,6 +67,8 @@ RED = "\033[91m"
 GREEN = "\033[92m"
 YELLOW = "\033[93m"
 RESET = "\033[0m"
+
+passed = 0
 
 print("Running module tests")
 for test in module_tests:
@@ -113,10 +120,13 @@ for test in module_tests:
       # include the directory and the line number in the message
       print(f"\n{RED}Unexpected error in regression/{test}/test.slf at line {unexpected_error}.{RESET}")
 
+  if len(error_lines) == 0 and len(should_not_have_thrown) == 0:
+    passed += 1
+
 
 # do the same thing but for small_tests
 
-print("Running individual tests")
+print("\nRunning individual tests")
 
 for test in small_tests:
   print(".", end="")
@@ -137,13 +147,13 @@ for test in small_tests:
 
   should_not_have_thrown = []
 
-  #print(f"test: {test}")
-  #print(f"error_lines (before): {error_lines}")
-
   # for each of the output lines, check if that's an error that should be thrown
   for i, line in enumerate(output_lines):
     # tests/errortests/one/demo.slf:72:
-    if f"{test}" in line and line.count(":") >= 2:
+
+    cropped_test = test[2:] if test.startswith("./") else test
+
+    if (test in line or cropped_test in line) and line.count(":") >= 2:
       # get the line number that the error was thrown on
       # the error line number is inbetween the first and second :
       error_line = int(line.split(":")[1])
@@ -172,3 +182,10 @@ for test in small_tests:
       # print that an error was thrown when it shouldn't have been
       # include the directory and the line number in the message
       print(f"{RED}     Unexpected error in {test} at line {unexpected_error}.{RESET}")
+
+  if len(error_lines) == 0 and len(should_not_have_thrown) == 0:
+    passed += 1
+
+
+print(f"\n{GREEN}{passed} tests passed.{RESET}")
+print(f"{RED}{len(small_tests) + len(module_tests) - passed} tests failed.{RESET}")
